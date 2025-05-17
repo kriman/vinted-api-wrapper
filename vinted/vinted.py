cc35faed029448c351874fb98cba9e2c157b2d97
kriman <kristof.kofias@protonmail.com>
2025-05-17 17:17:49 +0200

    I am not sure if this is the best way to add a raw response result functionality, but this is essential to navigate rapid schema changes on the platform. This will leave the data models detached from the actual data, but I want to move forward with accessing the data, without worrying about updating the models.

--- vinted/vinted.py
+++ vinted/vinted.py
@@ -1,10 +1,11 @@
 import time
 from copy import deepcopy
-from typing import List, Literal
+from typing import List, Literal, Union
 from urllib.parse import urlencode, urlparse, urlunparse
 
 import requests
 from dacite import from_dict
+from requests import Response
 
 from .endpoints import Endpoints
 from .models.base import VintedResponse
@@ -66,6 +67,31 @@ class Vinted:
             method=method, headers=self.headers, cookies=self.cookies, proxies=self.proxy, *args, **kwargs
         )
 
+    def _get_raw(
+        self,
+            endpoint: Endpoints,
+            format_values=None,
+            wanted_status_code: int = 200,
+            *args,
+            **kwargs,
+    ) -> Response:
+        if format_values:
+            url = self.api_url + endpoint.value.format(format_values)
+        else:
+            url = self.api_url + endpoint.value
+        response = self._call(method="get", url=url, *args, **kwargs)
+        if response.status_code != wanted_status_code and not kwargs.get("recursive"):
+            self.fetch_cookies()
+            return self._get_raw(
+                endpoint=endpoint,
+                format_values=format_values,
+                wanted_status_code=wanted_status_code,
+                recursive=True,
+                *args,
+                **kwargs,
+            )
+        return response
+
     def _get(
         self,
         endpoint: Endpoints,
@@ -75,22 +101,13 @@ class Vinted:
         *args,
         **kwargs,
     ):
-        if format_values:
-            url = self.api_url + endpoint.value.format(format_values)
-        else:
-            url = self.api_url + endpoint.value
-        response = self._call(method="get", url=url, *args, **kwargs)
-        if response.status_code != wanted_status_code and not kwargs.get("recursive"):
-            self.fetch_cookies()
-            return self._get(
-                endpoint=endpoint,
-                response_model=response_model,
-                format_values=format_values,
-                wanted_status_code=wanted_status_code,
-                recursive=True,
-                *args,
-                **kwargs,
-            )
+        response = self._get_raw(
+            endpoint=endpoint,
+            format_values=format_values,
+            wanted_status_code=wanted_status_code,
+            *args,
+            **kwargs
+        )
         try:
             json_response = response.json()
             return from_dict(response_model, json_response)
@@ -113,7 +130,8 @@ class Vinted:
         color_ids: int | List[int] = None,
         patterns_ids: int | List[int] = None,
         material_ids: int | List[int] = None,
-    ) -> SearchResponse:
+        raw: bool = False
+    ) -> Union[SearchResponse, Response]:
         params = {
             "page": page,
             "per_page": per_page,
@@ -132,20 +150,27 @@ class Vinted:
         }
         if url:
             params.update(parse_url_to_params(url))
-
+        if raw:
+            return self._get_raw(Endpoints.CATALOG_ITEMS, params=params)
         return self._get(Endpoints.CATALOG_ITEMS, SearchResponse, params=params)
 
     def search_users(
-        self, query: str, page: int = 1, per_page: int = 36
-    ) -> UserSearchResponse:
+        self, query: str, page: int = 1, per_page: int = 36, raw: bool = False
+    ) -> Union[UserSearchResponse, Response]:
         params = {"page": page, "per_page": per_page, "search_text": query}
+        if raw:
+            return self._get_raw(Endpoints.USERS, params=params)
         return self._get(Endpoints.USERS, UserSearchResponse, params=params)
 
-    def item_info(self, item_id: int) -> ItemsResponse:
+    def item_info(self, item_id: int, raw: bool = False) -> Union[ItemsResponse, Response]:
+        if raw:
+            return self._get_raw(Endpoints.ITEMS, item_id)
         return self._get(Endpoints.ITEMS, ItemsResponse, item_id)
 
-    def user_info(self, user_id: int, localize: bool = False) -> UserResponse:
+    def user_info(self, user_id: int, localize: bool = False, raw: bool = False) -> Union[UserResponse, Response]:
         params = {"localize": localize}
+        if raw:
+            return self._get_raw(Endpoints.USER, user_id, params=params)
         return self._get(Endpoints.USER, UserResponse, user_id, params=params)
 
     def user_items(
@@ -154,8 +179,11 @@ class Vinted:
         page: int = 1,
         per_page: int = 96,
         order: SortOption = "newest_first",
-    ) -> UserItemsResponse:
+        raw: bool = False
+    ) -> Union[UserItemsResponse, Response]:
         params = {"page": page, "per_page": per_page, "order": order}
+        if raw:
+            return self._get_raw(Endpoints.USER_ITEMS, user_id, params=params)
         return self._get(
             Endpoints.USER_ITEMS, UserItemsResponse, user_id, params=params
         )
@@ -166,22 +194,36 @@ class Vinted:
         page: int = 1,
         per_page: int = 20,
         by: Literal["all", "user", "system"] = "all",
-    ) -> UserFeedbacksResponse:
+        raw: bool = False
+    ) -> Union[UserFeedbacksResponse, Response]:
         params = {"user_id": user_id, "page": page, "per_page": per_page, "by": by}
+        if raw:
+            return self._get_raw(Endpoints.USER_FEEDBACKS, params=params)
         return self._get(Endpoints.USER_FEEDBACKS, UserFeedbacksResponse, params=params)
 
     def user_feedbacks_summary(
         self,
         user_id: int,
-    ) -> UserFeedbacksSummaryResponse:
+        raw: bool = False
+    ) -> Union[UserFeedbacksSummaryResponse, Response]:
         params = {"user_id": user_id}
+        if raw:
+            return self._get_raw(
+                Endpoints.USER_FEEDBACKS_SUMMARY,
+                params=params,
+            )
         return self._get(
             Endpoints.USER_FEEDBACKS_SUMMARY,
             UserFeedbacksSummaryResponse,
             params=params,
         )
 
-    def search_suggestions(self, query: str) -> SearchSuggestionsResponse:
+    def search_suggestions(self, query: str, raw: bool = False) -> Union[SearchSuggestionsResponse, Response]:
+        if raw:
+            return self._get_raw(
+                Endpoints.SEARCH_SUGGESTIONS,
+                params={"query": query},
+            )
         return self._get(
             Endpoints.SEARCH_SUGGESTIONS,
             SearchSuggestionsResponse,
@@ -195,7 +237,8 @@ class Vinted:
         brand_ids: int | List[int] = None,
         status_ids: int | List[int] = None,
         color_ids: int | List[int] = None,
-    ) -> FiltersResponse:
+        raw: bool = False
+    ) -> Union[FiltersResponse, Response]:
         params = {
             "search_text": query,
             "catalog_ids": catalog_ids,
@@ -204,9 +247,16 @@ class Vinted:
             "status_ids": status_ids,
             "color_ids": color_ids,
         }
+        if raw:
+            return self._get_raw(Endpoints.CATALOG_FILTERS, params=params)
         return self._get(Endpoints.CATALOG_FILTERS, FiltersResponse, params=params)
 
-    def catalogs_list(self) -> List[Catalog]:
+    def catalogs_list(self, raw: bool = False) -> Union[List[Catalog], Response]:
+        if raw:
+            self._get_raw(
+                Endpoints.CATALOG_INITIALIZERS,
+                params={"page": 1, "time": time.time()},
+            )
         data: InitializersResponse = self._get(
             Endpoints.CATALOG_INITIALIZERS,
             InitializersResponse,
